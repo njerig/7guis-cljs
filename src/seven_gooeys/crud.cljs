7c540a2bb3bfa6966be6940f04d7baef130cab2c
Njeri Gachoka <njerig00@gmail.com>
2020-12-11 21:54:51 -0800

    refactor #5

--- src/seven_gooeys/crud.cljs
+++ src/seven_gooeys/crud.cljs
@@ -3,6 +3,33 @@
     [reagent.core :as r]
     [clojure.string :as string]))
 
+(defn name-field [name-type new-name]
+  [:input {:type :text
+           :value (name-type @new-name)
+           :on-change #(swap! new-name assoc name-type (.. % -target -value))}])
+
+(defn button [value names selected new-name]
+  [:button {:value value
+            :on-click (case value
+                        "Create" (fn [e]
+                                   (swap! names conj (str (:surname @new-name) 
+                                                          ", " 
+                                                          (:name @new-name)))
+                                   (reset! new-name {:name "" :surname ""}))
+                        "Update" (fn [e]
+                                   (let [idx (.indexOf @names @selected)]
+                                     (swap! names assoc idx (str (:surname @new-name)
+                                                                 ", "
+                                                                 (:name @new-name)))))
+                        "Delete" (fn [e]
+                                   (reset! names (remove #(= @selected %) @names))))
+            :disabled (case value
+                        "Create"      (when (or (= (:name @new-name) "")
+                                                (= (:surname @new-name) ""))
+                                        true)
+                        (or "Update"
+                            "Delete") (when (= @selected "") true))} value])
+
 (defn component []
   (let [state (r/atom {:filter-prefix ""
                        :names ["Emil, Hans"
@@ -14,8 +41,7 @@
         filter-prefix (r/cursor state [:filter-prefix])
         names (r/cursor state [:names])
         new-name (r/cursor state [:new-name])
-        selected (r/cursor state [:selected])
-        new-name-string (str (:surname @new-name) ", " (:name @new-name))]
+        selected (r/cursor state [:selected])]
     (fn []
       [:div.crud-section
         [:h2 "CRUD"]
@@ -30,39 +56,22 @@
                          :margin-right "20px"}}
             (into [:select {:size 6
                             :id "listbox"
-                            :value @selected
                             :on-change #(reset! selected (.. % -target -value))}]
               (map
                 (fn [name] [:option {:key name} name])
-                (filter #(true? (string/starts-with? % (string/capitalize @filter-prefix))) @names)))]
+                (filter #(true? (string/starts-with? % (string/capitalize @filter-prefix))) 
+                        @names)))]
           [:div
             [:div
               [:div {:style {:width 88
                              :display "inline-block"}}
                 [:label "Name:"]]
-                [:input {:type :text
-                         :value (:name @new-name)
-                         :on-change #(swap! new-name assoc :name (.. % -target -value))}]]
+                [name-field :name new-name]]
             [:div
               [:div {:style {:display "inline-block"}}
                 [:label "Surname:"]]
-                [:input {:type :text
-                         :value (:surname @new-name)
-                         :on-change #(swap! new-name assoc :surname (.. % -target -value))}]]]]
+                [name-field :surname new-name]]]]
         [:div
-          [:button {:value "Create"
-                    :on-click (fn [e]
-                               (swap! names conj new-name-string)
-                               (reset! new-name {:name "" :surname ""}))
-                    :disabled (when (or (= (:name @new-name) "")
-                                       (= (:surname @new-name) ""))
-                               true)} "Create"]
-          [:button {:value "Update"
-                    :on-click (fn [e]
-                               (let [idx (.indexOf @names @selected)]
-                                 (swap! names assoc idx new-name-string)))
-                   :disabled (when (= @selected "") true)} "Update"]
-          [:button {:value "Delete"
-                    :on-click (fn [e]
-                               (reset! names (remove #(= @selected %) @names)))
-                    :disabled (when (= @selected "") true)} "Delete"]]])))
+          [button "Create" names selected new-name]
+          [button "Update" names selected new-name]
+          [button "Delete" names selected new-name]]])))
